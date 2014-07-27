4aeb44a9a22142e071968bbf126c8aa2cf3aaf29
Andrey Mironov <andrey.mir@gmail.com>
2014-07-27 23:24:22 +0400

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name afterpartyApp
+ * @description
+ * # afterpartyApp
+ *
+ * Main module of the application.
+ */
+angular
+    .module('afterpartyApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch',
+        'ui.sortable'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
